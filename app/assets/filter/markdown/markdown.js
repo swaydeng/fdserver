787cd69be41af4aa49f0119d5c2a810621e2a97e
bencode <bencode@163.com>
2013-07-31 15:36:31 +0800

    添加markdown目录

--- app/assets/filter/markdown/markdown.js
+++ app/assets/filter/markdown/markdown.js
@@ -4,6 +4,7 @@ var Reload = {
 	init: function() {
 		this.initPretty();
 		this.initReload();
+		this.initNav();
 	},
 
 
@@ -61,9 +62,70 @@ var Reload = {
 			end = html.indexOf('<!--end-markdown-body-->');
 
 		return html.substring(start, end);
+	},
+
+
+	initNav: function() {
+		this.refreshNav();	
+		$('body').on('reload', $.proxy(this, 'refreshNav'));
+	},
+
+
+	refreshNav: function() {
+		var self = this,
+			html = [];
+
+		var t = function(s) {
+			html.push(s);
+		};
+
+		t('<ul class="navs">');
+
+		$('h2,h3', '#doc').each(function() {
+			var elm = $(this);
+			
+			self.createHash(elm);
+
+			var cls = elm.is('h2') ? 'item' : 'child';
+
+			t(self.format('<li class="{0}">', [cls]));
+				t(self.createLink(elm));
+			t('</li>');
+		});
+
+		html.push('</ul>');
+
+		$('div.nav-container', '#doc').html(html.join(''));
+	},
+
+
+	hashInfo: function(node) {
+		var text = node.text();	
+		return {
+			hash: text.replace(/\s+/g, '-'),
+			name: text
+		};
+	},
+
+
+	createHash: function(node) {
+		var o = this.hashInfo(node);
+		node.prepend(this.format('<a name="{hash}"></a>', o));
+	},
+
+
+	createLink: function(node) {
+		var o = this.hashInfo(node);
+		return this.format('<a href="#{hash}">{name}</a>', o);
+	},
+
+
+	format:	function(str, data) {
+		return str.replace(/\{(\w+)\}/g, function(r, m) {
+			return data[m] !== undefined && data[m] !== null ? 
+					data[m] : '{' + m + '}';
+		});
 	}
-
-
 	
 };
 
